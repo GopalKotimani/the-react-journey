1a8c159108ce452ac0387187333020619ca7dba0
Gopal-19 <60785555+Gopal-19@users.noreply.github.com>
2025-09-02 12:26:11 +0530

    Progress Bar

---
+++ src/problems/Progressbar/ProgressBar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+const ProgressBar = () => {
+    const [progress, setProgress] = useState(0);
+
+    const updateProgress = (value) => {
+        setProgress((prev) => Math.max(0, Math.min(100, prev +value)))
+    }
+    return (
+        <div className='container'>
+            <h2 className='text-2xl m-6'>Progress Bar</h2>
+            <div className='outer'>
+                <div className='inner'
+                    style={{
+                        width: `${progress}%`
+                    }}
+                >
+                    <span className='text-white'>{progress}%</span>
+                </div>
+            </div>
+            <div className='m-5 space-x-3'>
+                <button
+                    className='bg-gray-500 border w-20 rounded-sm cursor-pointer'
+                    onClick={() => updateProgress(-10)}
+                >
+                    -10%
+                </button>
+                <button
+                    className='bg-gray-500 border w-20 rounded-sm cursor-pointer'
+                    onClick={() => updateProgress(+10)}
+                >
+                    +10%
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProgressBar
